a241a466e30593dbbafc565f3c2d685475a26ff6
miyasitatakeru <k021g1291@g.neec.ac.jp>
2022-12-06 10:35:10 +0900

    02_01.テクスチャと法線

--- Object3d.cpp
+++ Object3d.cpp
@@ -403,14 +403,15 @@ void Object3d::CreateModel()
 	// ファイルストリーム
 	std::fstream file;
 	// .objファイルを開く
-	file.open("Resources/triangle/triangle.obj");
+	//file.open("Resources/triangle/triangle.obj");
+	file.open("Resources/triangle_tex/triangle_tex.obj");
 	// ファイルオープン失敗をチェック
 	if (file.fail()) {
 		assert(0);
 	}
 	vector<XMFLOAT3>positions;  // 視点座標
 	vector<XMFLOAT3>normals;   // 法線ベクトル
-	vector<XMFLOAT3>texcoords; // テクスチャUV
+	vector<XMFLOAT2>texcoords; // テクスチャUV
 	// 1行ずつ読み込む
 	string line;
 	while (getline(file, line)) {
@@ -421,7 +422,7 @@ void Object3d::CreateModel()
 		string key;
 		getline(line_stream, key, ' ');
 
-		//先頭文字列がvなら頂点座標
+		//先頭文字列がvzなら頂点座標
 		if (key == "v") {
 			//X,Y,Z座標読み込み
 			XMFLOAT3 position{};
@@ -431,10 +432,36 @@ void Object3d::CreateModel()
 			//座標データに追加
 			positions.emplace_back(position);
 			//頂点データに追加
-			VertexPosNormalUv vertex{};
+			/*VertexPosNormalUv vertex{};
 			vertex.pos = position;
-			vertices.emplace_back(vertex);
+			vertices.emplace_back(vertex);*/
 		}
+		// 先頭文字列がvtなら頂点座標
+		if (key == "vt") {
+			// U,V座標読み込み
+			XMFLOAT2 texcoord{};
+			line_stream >> texcoord.x;
+			line_stream >> texcoord.y;
+			// V方向反転
+			texcoord.y = 1.0f - texcoord.y;
+			//テクスチャ座標データに追加
+			texcoords.emplace_back(texcoord);
+			//頂点データに追加
+			/*VertexPosNormalUv vertex{};
+			vertex.pos = position;
+			vertices.emplace_back(vertex);*/
+		}
+		//先頭文字列がvnなら法線ベクトル
+		if (key == "vn") {
+			//X,Y,Z座標読み込み
+			XMFLOAT3 normal{};
+			line_stream >> normal.x;
+			line_stream >> normal.y;
+			line_stream >> normal.z;
+			// 法線ベクトルデータに追加
+			normals.emplace_back(normal);
+		}
+
 		//先頭文字列がfならポリゴン(三角形)
 		if (key == "f") {
 			//半角スペース区切りで行の続きの行を読み込む
@@ -442,10 +469,20 @@ void Object3d::CreateModel()
 			while (getline(line_stream, index_string, ' ')) {
 				//頂点インデックス1個分の文字列をストリームに変換して解析しやすくする
 				std::istringstream index_stream(index_string);
-				unsigned short indexPosition;
+				unsigned short indexPosition, indexNormal, indexTexcoord;
 				index_stream >> indexPosition;
-				//頂点インデックスに追加
-				indices.emplace_back(indexPosition - 1);
+				index_stream.seekg(1, ios_base::cur); // スラッシュを飛ばす
+				index_stream >> indexTexcoord;
+				index_stream.seekg(1, ios_base::cur); // スラッシュを飛ばす
+				index_stream >> indexNormal;
+				//頂点データの追加
+				VertexPosNormalUv vertex{};
+				vertex.pos = positions[indexPosition - 1];
+				vertex.normal = normals[indexNormal - 1];
+				vertex.uv = texcoords[indexTexcoord - 1];
+				vertices.emplace_back(vertex);
+				//インデックスデータの追加
+				indices.emplace_back((unsigned short)indices.size());
 			}
 		}
 	}
@@ -454,8 +491,6 @@ void Object3d::CreateModel()
 	file.close();
 
 	HRESULT result = S_FALSE;
-
-	// 01_03.3Dモデル読み込み 資料13から
 
 	//std::vector<VertexPosNormalUv> realVertices;
 	//// 頂点座標の計算（重複あり）
